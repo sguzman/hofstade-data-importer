f7ec8143b8f548b62cf3537f763d106f218af30c
Salvador Guzman Jr <guzmansalv@gmail.com>
2020-04-16 19:00:08 -0700

    connection to db working

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+import atexit
+import psycopg2
+
+
+def con() -> psycopg2:
+    print('Connecting to PostgreSQL DB')
+    conn: psycopg2 = \
+        psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='postgres')
+
+    def clean_up() -> None:
+        conn.close()
+        print('Closing connection', conn)
+
+    atexit.register(clean_up)
+    return conn
+
+
 def main() -> None:
+    con()
     print('hello')
 
 
